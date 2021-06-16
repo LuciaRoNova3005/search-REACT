d4abf5b06cf0a9ee06b7f39045580edf7452ebe7
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-06-16 11:25:55 +0200

    Import hashrouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './stylesheets/index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./stylesheets/index.css";
+import App from "./components/App";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </HashRouter>,
+  document.getElementById("root")
 );
-
-

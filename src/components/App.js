1ef89f201542fe3ca404c41389a5d1b222211b0d
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-06-16 08:43:50 +0200

    Local storage

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,34 @@
 import "../stylesheets/App.css";
 import React, { useState, useEffect } from "react";
 import Logo from "../img/Logo.png";
+import ls from "../Services/local-storage";
 import getApiData from "../Services/Api";
 import CharactersList from "../components/CharactersList";
 import Filters from "./Filters";
 
 const App = () => {
   /*Un array vacío para definir el estado inicial
-   */
-  const [characters, setcharacters] = useState([]);
+   pasa una key y un valor defaulft*/
+  const [characters, setcharacters] = useState(ls.get("characters", []));
   /*Empieza ejecutarse en la parte de montaje y es lo que hace que salga los datos
    */
   useEffect(() => {
-    getApiData().then((charactersData) => {
-      setcharacters(charactersData);
-    });
+    /*Si el array esta vacío no accedes al locastorage
+     */
+    if (characters.length === 0) {
+      getApiData().then((charactersData) => {
+        setcharacters(charactersData);
+      });
+    }
   }, []);
+  /*Parámetro que nos indica cuando debe ejecutarse el useEffect
+   */
+
+  useEffect(() => {
+    ls.set("characters", characters);
+  }, [characters]);
+  /*Este Useeffect nos guarda en localsotrage cuando los personajes cambian,se ejecuta cuando characters cambia
+   */
 
   return (
     <>
